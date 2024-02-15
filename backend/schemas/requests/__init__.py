bdad845c5b97e0c006ba67720d23b7aed846a983
Joen Tan <joentze01@gmail.com>
2024-02-15 10:28:55 +0800

    refactor: moved conversation response to __init__.py

---
+++ backend/schemas/requests/__init__.py
@@ -0,0 +1,14 @@
+"""typing for Conversations"""
+from pydantic import BaseModel, Field
+
+
+class ConversationPOST(BaseModel):
+    """type for ConversationPOST"""
+    name: str = Field(max_length=200)
+    params: object = Field(...)
+
+
+class ConversationPUT(BaseModel):
+    """type for ConversationPOST"""
+    name: str = Field(max_length=200)
+    params: object = Field(...)
