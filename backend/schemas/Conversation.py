0b1bb48fc1a4d07e910f0b4b66809525cd3a70df
Joen Tan <joentze01@gmail.com>
2024-02-15 10:03:18 +0800

    feat: added conversation post type:

--- backend/schemas/Conversation.py
+++ backend/schemas/Conversation.py
@@ -6,5 +6,5 @@ class Conversation(BaseModel):
     """type for Conversation"""
     id: UUID4 = Field(...)
     name: str = Field(max_length=200)
-    params: dict = Field(...)
+    params: object = Field(...)
     tokens: int = Field(ge=0)
---
+++ backend/schemas/ConversationPOST.py
@@ -0,0 +1,8 @@
+"""typing for Conversations"""
+from pydantic import BaseModel, Field
+
+
+class ConversationPOST(BaseModel):
+    """type for ConversationPOST"""
+    name: str = Field(max_length=200)
+    params: object = Field(...)
