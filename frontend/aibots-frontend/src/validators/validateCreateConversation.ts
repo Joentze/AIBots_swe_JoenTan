ddbda94e3d7e25b0cfa95697e8eee1ef844aaef8
Joen Tan <joentze01@gmail.com>
2024-02-18 22:52:07 +0800

    refactor: moved docker ignore

--- frontend/aibots-frontend/src/validators/validateCreateConversation.ts
+++
@@ -1,8 +0,0 @@
-export const validateCreateConversation = (
-  conversationName: string,
-  conversationParams: string
-): void => {
-  if (conversationName === undefined || conversationName === "")
-    throw new Error("The conversation name cannot be blank!");
-  JSON.parse(conversationParams);
-};
